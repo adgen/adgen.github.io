d99ab8c354e67080c1acc42b2f52029d2940c0f6
juff <vladimir.voytsekhovskiy@gmail.com>
2017-06-12 23:42:41 +0300

    ready

--- adgen.js
+++ adgen.js
@@ -1,29 +1,35 @@
 window.onload = () => {
 
 
-    const adWrapper = document.getElementById("adWrapper");
-    adWrapper.style.visibility = "hidden";
-    const selector = document.getElementById("file");
-    selector.style.visibility = "hidden";
-    const selectButton = document.getElementById("select");
-    selectButton.addEventListener("click", () => {
+    const adWrapper = document.getElementById('adWrapper');
+    adWrapper.style.visibility = 'hidden';
+    const selector = document.getElementById('file');
+    selector.style.visibility = 'hidden';
+    const selectButton = document.getElementById('select');
+    selectButton.addEventListener('click', () => {
         selector.click();
     });
-    document.getElementById("file").addEventListener("change", () => {
-        adWrapper.style.visibility = "visible";
+    document.getElementById('file').addEventListener('change', () => {
+        adWrapper.style.visibility = 'visible';
         document.getElementById('button').style.visibility = 'visible';
         document.getElementById('select').style.visibility = 'hidden';
+        const oldPrice = document.getElementById('oldPrice');
+        const newPrice = document.getElementById('newPrice');
+        const discount = document.getElementById('discountBlock');
+        oldPrice.innerHTML = document.getElementById('oldPriceText').value;
+        newPrice.innerHTML = document.getElementById('newPriceText').value;
+        discount.innerHTML = Math.floor((1-newPrice.innerHTML / oldPrice.innerHTML)*100) + '%';
         PreviewImage();
     });
 
     PreviewImage = () => {
         const fileReader = new FileReader();
-        fileReader.readAsDataURL(document.getElementById("file").files[0]);
+        fileReader.readAsDataURL(document.getElementById('file').files[0]);
         fileReader.addEventListener('load', function (oFREvent) {
-            document.getElementById("srcImage").style.backgroundImage = 'url(' + oFREvent.target.result + ')';
-            html2canvas(document.getElementById("adWrapper"), {
+            document.getElementById('srcImage').style.backgroundImage = 'url(' + oFREvent.target.result + ')';
+            html2canvas(document.getElementById('adWrapper'), {
                 onrendered: (canvas) => {
-                    document.getElementById("canvasContainer").appendChild(canvas);
+                    document.getElementById('canvasContainer').appendChild(canvas);
                     document.body.removeChild(adWrapper);
                 },
                 width: 1920,
@@ -34,14 +40,14 @@ window.onload = () => {
 
     };
 
-    const button = document.getElementById("button");
-    button.addEventListener("click", () => {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
 
-        const canvas = document.getElementsByTagName("canvas")[0];
+        const canvas = document.getElementsByTagName('canvas')[0];
 
         downloadCanvas = () => {
-            canvas.setAttribute("id", "canvas");
-            const link = document.createElement("a");
+            canvas.setAttribute('id', 'canvas');
+            const link = document.createElement('a');
             document.body.appendChild(link);
             link.href = canvas.toDataURL();
             link.download = 'image.png';
